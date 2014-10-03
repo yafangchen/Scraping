f1672c44acbf5b29beb0ad26ec138826988cec85
weihengchen <weihengchen89@gmail.com>
2014-10-02 22:11:33 -0500

    add description of function

--- Scraping.py
+++ Scraping.py
@@ -1,5 +1,23 @@
 import urllib2
 
+def url2file(url, filename):
+    """Scrape content from url, and save to filename
+
+    Retrieve content from url, save to file.
+
+    Args:
+        url: a string of website. e.g. http://google.com
+        filename: the absolute name of file.
+
+    Returns:
+        None
+
+    Raises:
+        IOError: An error occurred accessing file.
+        HTTPError: An error occurred accessing url.
+    """
+    pass
+
 if __name__ == '__main__':
     file="downloaded_file.html"
     url="http://python.org/"
